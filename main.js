1c4bb014fc30cbedf9aaafa503e7b333f7c84663
David Kelly <treellane@yahoo.co.uk>
2019-03-10 12:06:45 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const baseURL = "https://swapi.co/api/";  
+                                            
+                                            
+function getData(type, cb) {                        
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");     
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));              
+        }                                                  
+    };
+}
+
+// HERE WE START TO DISPLAT OUR DATA IN A TABLE...
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+    
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);   
+    });                                         
+
+    return `<tr>${tableHeaders}</tr>`;
+    
+}
+
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");   
+    
+    el.innerHTML = ""                            
+                                                
+    getData(type, function(data) {
+        data = data.results;
+        
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+           
+            var dataRow = [];
+            
+            Object.keys(item).forEach(function(key) {
+             
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15); // this var will cust lines off at 15 character to clean up the screen
+             
+                dataRow.push(`<td>${truncatedData}</td>`)
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`); 
+        });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+    });
+    
+}
+
+
